476dfb853952a1395117b755a30ddf1c5b260d1f
tommystarlit <tommystarlit@gmail.com>
2019-12-06 21:48:20 +0100

    Name list added

--- clashtestbuilder.py
+++ clashtestbuilder.py
@@ -1,5 +1,17 @@
-print("Hello World")
+import sys
+src = sys.argv[1]
+# src = "/home/tommystarlit/Programowanie/GitRepo/clashTestBuilder/CNEB_cTests.xml"
 
+import xml.etree.ElementTree as ET
+root = ET.parse(src).getroot()
+
+ssets = []
+for sset in root.findall('selectionsets/selectionset'):
+    value = sset.get('name')
+    ssets.append(value)
+
+print(ssets)
+    # print(value)
 # - provide search set file
 # - copy ss file to as new_name
 # - open ss file
