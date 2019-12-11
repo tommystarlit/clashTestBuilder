619013cd4f2ba93fb228fcddcc5a9c99f56eeee6
tommystarlit <tommystarlit@gmail.com>
2019-12-11 22:31:37 +0100

    update

--- clashtestbuilder.py
+++ clashtestbuilder.py
@@ -1,12 +1,16 @@
 import sys
 src = sys.argv[1]
 # src = "/home/tommystarlit/Programowanie/GitRepo/clashTestBuilder/CNEB_cTests.xml"
+dst = sys.argv[2]
+dst = "/home/tommystarlit/Programowanie/GitRepo/clashTestBuilder/template.xml"
 
 import xml.etree.ElementTree as ET
-root = ET.parse(src).getroot()
+import copy
+
+sroot = ET.parse(src).getroot()
 
 ssets = []
-for sset in root.findall('selectionsets/selectionset'):
+for sset in sroot.findall('selectionsets/selectionset'):
     value = sset.get('name')
     ssets.append(value)
 
@@ -21,14 +25,21 @@ for sset in ssets:
         ctests.append(ctest)
 
 # print(ctests)
+droot = ET.parse(dst).getroot()
+# print((droot.findall('batchtest/clashtests/clashtest')[2].get('name')))
+dlen = len(droot.findall('batchtest/clashtests/clashtest'))
+
+while dlen < len(ctests):
+    droot[0][0].append(copy.deepcopy(droot[0][0][0]))
+#     file = ET.fromstring(s)
+# b = file.find("b")
+# for c in file.findall(".//c"):
+#     dupe = copy.deepcopy(c) #copy <c> node
+#     b.append(dupe) #insert the new node
 
 
-
-    # print(value)
-# - provide search set file
-# - copy ss file to as new_name
-# - open ss file
 # - scrap search set names
+# - file name from parameter
 # - create test sets
 # - create tests
 # - append to file
